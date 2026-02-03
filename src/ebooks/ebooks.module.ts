01924088cd1db8cdf83c80ebf37d5847ff41a089
AbdulMalek-swe <abdulmalek.swe.585@gmail.com>
2026-02-03 10:17:12 +0600

    fix

--- src/ebooks/ebooks.module.ts
+++ src/ebooks/ebooks.module.ts
@@ -1,18 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { MulterModule } from '@nestjs/platform-express';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 import { EbooksController } from './ebooks.controller';
 import { EbooksService } from './ebooks.service';
 import { Ebook } from './entities/ebook.entity';
 
 @Module({
-   imports: [
+  imports: [
     TypeOrmModule.forFeature([Ebook]), // This provides the EbookRepository
   ],
   controllers: [EbooksController],
   providers: [EbooksService],
   exports: [EbooksService], // Export if needed by other modules
 })
-export class EbooksModule {}
+export class EbooksModule { }
